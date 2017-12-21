552cd9d0921619cbf1997346b9931603476a7d9c
kangxi <kang.xi@gtdollar.com>
2017-12-21 12:21:16 +0800

    Add config folder

--- index.js
+++ index.js
@@ -2,133 +2,140 @@ var request = require('request');
 var cron = require('node-cron');
 var async = require('async');
 
-var qryptos = require('./qryptos');
-var config = require('./config');
+var client = require('./qryptos');
 
-var interval = 0.00000001;
+var config = process.argv.slice(2);
+if (config.length) {
+    config = require('./' + config);
 
-cron.schedule('*/5 * * * * *', function () {
-    async.parallel({
-        coinmarketcap: function (callback) {
-            request.get({
-                url: 'https://api.coinmarketcap.com/v1/ticker/?convert=ETH',
-                json: true
-            }, function (error, response, body) {
-                try {
-                    callback(null, body);
-                } catch (e) {
-                    return callback(e);
-                }
-            });
-        },
-        order: function (callback) {
-            qryptos.order(config.id, function (data) {
-                callback(null, data);
-            });
-        },
-        book: function (callback) {
-            request.get({
-                url: 'https://api.qryptos.com/products/' + config.product_id + '/price_levels',
-                json: true
-            }, function (error, response, body) {
-                try {
-                    callback(null, body);
-                } catch (e) {
-                    return callback(e);
-                }
-            });
-        }
-    }, function (err, results) {
-        if (err) {
-            console.log(err);
-        } else {
-            if (results.order.status != 'live') {
-                console.log('This is not a live order');
-                process.exit();
+    var qryptos = new client(config);
+
+    var interval = 0.00000001;
+    cron.schedule('*/5 * * * * *', function () {
+        async.parallel({
+            coinmarketcap: function (callback) {
+                request.get({
+                    url: 'https://api.coinmarketcap.com/v1/ticker/?convert=ETH',
+                    json: true
+                }, function (error, response, body) {
+                    try {
+                        callback(null, body);
+                    } catch (e) {
+                        return callback(e);
+                    }
+                });
+            },
+            order: function (callback) {
+                qryptos.order(config.id, function (response) {
+                    callback(null, response);
+                });
+            },
+            book: function (callback) {
+                request.get({
+                    url: 'https://api.qryptos.com/products/' + config.product_id + '/price_levels',
+                    json: true
+                }, function (error, response, body) {
+                    try {
+                        callback(null, body);
+                    } catch (e) {
+                        return callback(e);
+                    }
+                });
             }
-
-            var coinmarketcap_price = results.coinmarketcap.find(item => item.symbol == config.symbol)['price_' + config.currency.toLowerCase()];
-            var current_price = results.order.price;
-            var quantity = results.order.quantity;
-
-            var book = results.book;
-
-            var buy = book.buy_price_levels[0];
-            var buy_second = book.buy_price_levels[1];
-            var buy_price = parseFloat(buy[0]);
-            var buy_quantity = parseFloat(buy[1]);
-
-            var sell = book.sell_price_levels[0];
-            var sell_second = book.sell_price_levels[1];
-            var sell_price = parseFloat(sell[0]);
-            var sell_quantity = parseFloat(sell[1]);
-
-            if (config.side == 'buy') {
-                //buy
-                var profit = (sell_price - current_price) / current_price;
-                console.log('profit: ' + (profit * 100).toFixed(2) + '%');
-                if (current_price < coinmarketcap_price && profit >= config.profit) {
-                    //check status
-                    var change = false;
-                    if (current_price >= buy_price && quantity == buy_quantity) {
-                        //check second order
-                        var difference = (current_price - buy_second[0]).toFixed(8);
-                        if (difference > interval) {
-                            change = true;
-                            current_price = parseFloat(buy_second[0]) + interval;
-                        }
-                    } else {
-                        //edit order to be first order
-                        change = true;
-                        current_price = buy_price + interval;
-                    }
-
-                    if (change) {
-                        current_price = current_price.toFixed(8);
-                        qryptos.editorder(config.id, current_price, quantity, function (data) {
-                            console.log('Change price: ' + current_price + ', profit: ' + (profit * 100).toFixed(2) + '%');
-                        });
-                    }
-                } else {
-                    //cancel order
-                    qryptos.cancelorder(config.id, function (response) {
-                        console.log('Cancel order ' + config.id);
-                        process.exit();
-                    });
-                }
+        }, function (err, results) {
+            if (err) {
+                console.log(err);
             } else {
-                //sell
-                var profit = (sell_price - config.cost) / config.cost;
-                console.log('profit: ' + (profit * 100).toFixed(2) + '%');
-                if (current_price > coinmarketcap_price && profit >= config.profit) {
-                    var change = false;
-                    if (current_price <= sell_price && quantity == sell_quantity) {
-                        //check second order
-                        var difference = (sell_second[0] - current_price).toFixed(8);
-                        if (difference > interval) {
+                if (results.order.status != 'live') {
+                    console.log('This is not a live order');
+                    process.exit();
+                }
+
+                var coinmarketcap_price = results.coinmarketcap.find(item => item.symbol == config.symbol)['price_' + config.currency.toLowerCase()];
+                var current_price = results.order.price;
+                var quantity = results.order.quantity;
+
+                var book = results.book;
+
+                var buy = book.buy_price_levels[0];
+                var buy_second = book.buy_price_levels[1];
+                var buy_price = parseFloat(buy[0]);
+                var buy_quantity = parseFloat(buy[1]);
+
+                var sell = book.sell_price_levels[0];
+                var sell_second = book.sell_price_levels[1];
+                var sell_price = parseFloat(sell[0]);
+                var sell_quantity = parseFloat(sell[1]);
+
+                if (config.side == 'buy') {
+                    //buy
+                    var profit = (sell_price - current_price) / current_price;
+                    console.log('profit: ' + (profit * 100).toFixed(2) + '%');
+                    if (current_price < coinmarketcap_price && profit >= config.profit) {
+                        //check status
+                        var change = false;
+                        if (current_price >= buy_price && quantity == buy_quantity) {
+                            //check second order
+                            var difference = (current_price - buy_second[0]).toFixed(8);
+                            if (difference > interval) {
+                                change = true;
+                                current_price = parseFloat(buy_second[0]) + interval;
+                            }
+                        } else {
+                            //edit order to be first order
                             change = true;
-                            current_price = parseFloat(sell_second[0]) - interval;
+                            current_price = buy_price + interval;
+                        }
+
+                        if (change) {
+                            current_price = current_price.toFixed(8);
+                            qryptos.editorder(config.id, current_price, quantity, function (data) {
+                                console.log('Change price: ' + current_price + ', profit: ' + (profit * 100).toFixed(2) + '%');
+                            });
                         }
                     } else {
-                        //edit order to be first order
-                        change = true;
-                        current_price = sell_price - interval;
-                    }
-
-                    if (change) {
-                        current_price = current_price.toFixed(8);
-                        qryptos.editorder(config.id, current_price, quantity, function (data) {
-                            console.log('Change price: ' + current_price + ', profit: ' + (profit * 100).toFixed(2) + '%');
+                        //cancel order
+                        qryptos.cancelorder(config.id, function (response) {
+                            console.log('Cancel order ' + config.id);
+                            process.exit();
                         });
                     }
                 } else {
-                    //cancel order
-                    qryptos.cancelorder(config.id, function (response) {
-                        console.log('Cancel order ' + config.id);
-                        process.exit();
-                    });
+                    //sell
+                    var profit = (sell_price - config.cost) / config.cost;
+                    console.log('profit: ' + (profit * 100).toFixed(2) + '%');
+                    if (current_price > coinmarketcap_price && profit >= config.profit) {
+                        var change = false;
+                        if (current_price <= sell_price && quantity == sell_quantity) {
+                            //check second order
+                            var difference = (sell_second[0] - current_price).toFixed(8);
+                            if (difference > interval) {
+                                change = true;
+                                current_price = parseFloat(sell_second[0]) - interval;
+                            }
+                        } else {
+                            //edit order to be first order
+                            change = true;
+                            current_price = sell_price - interval;
+                        }
+
+                        if (change) {
+                            current_price = current_price.toFixed(8);
+                            qryptos.editorder(config.id, current_price, quantity, function (data) {
+                                console.log('Change price: ' + current_price + ', profit: ' + (profit * 100).toFixed(2) + '%');
+                            });
+                        }
+                    } else {
+                        //cancel order
+                        qryptos.cancelorder(config.id, function (response) {
+                            console.log('Cancel order ' + config.id);
+                            process.exit();
+                        });
+                    }
                 }
             }
-        }
+        });
     });
-});
+} else {
+    console.log('Can not find config file');
+}
--- qryptos.js
+++ qryptos.js
@@ -1,164 +1,160 @@
 var request = require('request');
 var jwt = require('jsonwebtoken');
-var config = require('./config');
 
-var
-  token_id = config.token_id,
-  user_secret = config.user_secret,
-  baseRequest = request.defaults({
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Quoine-API-Version': '2'
-    },
-    baseUrl: 'https://api.qryptos.com'
-  });
+var self;
 
-function getOptions(verb, url, payload) {
-  var signature = jwt.sign(payload, user_secret);
-  var body = '';
+var qryptos = function (config) {
+	self = this;
 
-  if (verb == 'PUT' || verb == 'POST') {
-    body = JSON.stringify(payload);
-  }
-
-  return {
-    url: url,
-    headers: {
-      'X-Quoine-Auth': signature
-    },
-    body: body
-  };
+	this.token_id = config.token_id;
+	this.user_secret = config.user_secret;
+	this.baseRequest = request.defaults({
+		headers: {
+			'Content-Type': 'application/json',
+			'X-Quoine-API-Version': '2'
+		},
+		baseUrl: 'https://api.qryptos.com'
+	});
 }
 
-var balances = function (callback) {
-  var verb = 'GET';
-  var url = '/accounts/balance';
-  var payload = {
-    'path': url,
-    'nonce': Date.now(),
-    'token_id': token_id
-  };
+function getOptions(verb, url, payload) {
+	var signature = jwt.sign(payload, self.user_secret);
+	var body = '';
 
-  var options = getOptions(verb, url, payload);
+	if (verb == 'PUT' || verb == 'POST') {
+		body = JSON.stringify(payload);
+	}
 
-  baseRequest.get(options, function (error, response, body) {
-    callback(JSON.parse(body));
-  });
+	return {
+		url: url,
+		headers: {
+			'X-Quoine-Auth': signature
+		},
+		body: body
+	};
+}
+
+qryptos.prototype.balances = function (callback) {
+	var verb = 'GET';
+	var url = '/accounts/balance';
+	var payload = {
+		'path': url,
+		'nonce': Date.now(),
+		'token_id': this.token_id
+	};
+
+	var options = getOptions(verb, url, payload);
+
+	this.baseRequest.get(options, function (error, response, body) {
+		callback(JSON.parse(body));
+	});
 };
 
-var executions = function (product_id, callback) {
-  var verb = 'GET';
-  var url = '/executions/me?product_id=' + product_id;
-  var payload = {
-    'path': url,
-    'nonce': Date.now(),
-    'token_id': token_id
-  };
+qryptos.prototype.trades = function (product_id, callback) {
+	var verb = 'GET';
+	var url = '/executions/me?product_id=' + product_id;
+	var payload = {
+		'path': url,
+		'nonce': Date.now(),
+		'token_id': this.token_id
+	};
 
-  var options = getOptions(verb, url, payload);
+	var options = getOptions(verb, url, payload);
 
-  baseRequest.get(options, function (error, response, body) {
-    callback(JSON.parse(body));
-  });
+	this.baseRequest.get(options, function (error, response, body) {
+		callback(JSON.parse(body));
+	});
 };
 
-var order = function (id, callback) {
-  var verb = 'GET';
-  var url = '/orders/' + id;
-  var payload = {
-    'path': url,
-    'nonce': Date.now(),
-    'token_id': token_id
-  };
+qryptos.prototype.order = function (id, callback) {
+	var verb = 'GET';
+	var url = '/orders/' + id;
+	var payload = {
+		'path': url,
+		'nonce': Date.now(),
+		'token_id': this.token_id
+	};
 
-  var options = getOptions(verb, url, payload);
+	var options = getOptions(verb, url, payload);
 
-  baseRequest.get(options, function (error, response, body) {
-    callback(JSON.parse(body));
-  });
+	this.baseRequest.get(options, function (error, response, body) {
+		callback(JSON.parse(body));
+	});
 };
 
-var orders = function (product_id, callback) {
-  var verb = 'GET';
-  var url = '/orders?&status=live&product_id=' + product_id;
-  var payload = {
-    'path': url,
-    'nonce': Date.now(),
-    'token_id': token_id
-  };
+qryptos.prototype.liveorders = function (product_id, callback) {
+	var verb = 'GET';
+	var url = '/orders?&status=live&product_id=' + product_id;
+	var payload = {
+		'path': url,
+		'nonce': Date.now(),
+		'token_id': this.token_id
+	};
 
-  var options = getOptions(verb, url, payload);
+	var options = getOptions(verb, url, payload);
 
-  baseRequest.get(options, function (error, response, body) {
-    callback(JSON.parse(body));
-  });
+	this.baseRequest.get(options, function (error, response, body) {
+		callback(JSON.parse(body));
+	});
 };
 
-var createOrder = function (product_id, amount, price, side, callback) {
-  var verb = 'PUT';
-  var url = '/orders/';
-  var payload = {
-    'path': url,
-    'nonce': Date.now(),
-    'token_id': token_id,
-    'order': {
-      'order_type': 'limit',
-      'product_id': product_id,
-      'side': side,
-      'quantity': amount,
-      'price': price
-    }
-  };
+qryptos.prototype.neworder = function (product_id, amount, price, side, callback) {
+	var verb = 'PUT';
+	var url = '/orders/';
+	var payload = {
+		'path': url,
+		'nonce': Date.now(),
+		'token_id': this.token_id,
+		'order': {
+			'order_type': 'limit',
+			'product_id': product_id,
+			'side': side,
+			'quantity': amount,
+			'price': price
+		}
+	};
 
-  var options = getOptions(verb, url, payload);
+	var options = getOptions(verb, url, payload);
 
-  baseRequest.post(options, function (error, response, body) {
-    callback(JSON.parse(body));
-  });
+	this.baseRequest.post(options, function (error, response, body) {
+		callback(JSON.parse(body));
+	});
 };
 
-var cancelOrder = function (id, callback) {
-  var verb = 'GET';
-  var url = '/orders/' + id + '/cancel';
-  var payload = {
-    'path': url,
-    'nonce': Date.now(),
-    'token_id': token_id
-  };
+qryptos.prototype.cancelorder = function (id, callback) {
+	var verb = 'GET';
+	var url = '/orders/' + id + '/cancel';
+	var payload = {
+		'path': url,
+		'nonce': Date.now(),
+		'token_id': this.token_id
+	};
 
-  var options = getOptions(verb, url, payload);
+	var options = getOptions(verb, url, payload);
 
-  baseRequest.put(options, function (error, response, body) {
-    callback(JSON.parse(body));
-  });
+	this.baseRequest.put(options, function (error, response, body) {
+		callback(JSON.parse(body));
+	});
 };
 
-var editOrder = function (id, price, quantity, callback) {
-  var verb = 'PUT';
-  var url = '/orders/' + id;
-  var payload = {
-    'path': url,
-    'nonce': Date.now(),
-    'token_id': token_id,
-    'order': {
-      'quantity': quantity,
-      'price': price
-    }
-  };
+qryptos.prototype.editorder = function (id, price, quantity, callback) {
+	var verb = 'PUT';
+	var url = '/orders/' + id;
+	var payload = {
+		'path': url,
+		'nonce': Date.now(),
+		'token_id': this.token_id,
+		'order': {
+			'quantity': quantity,
+			'price': price
+		}
+	};
 
-  var options = getOptions(verb, url, payload);
+	var options = getOptions(verb, url, payload);
 
-  baseRequest.put(options, function (error, response, body) {
-    callback(JSON.parse(body));
-  });
+	this.baseRequest.put(options, function (error, response, body) {
+		callback(JSON.parse(body));
+	});
 };
 
-module.exports = {
-  balances: balances,
-  trades: executions,
-  liveorders: orders,
-  order: order,
-  neworder: createOrder,
-  cancelorder: cancelOrder,
-  editorder: editOrder
-};
+module.exports = qryptos;
